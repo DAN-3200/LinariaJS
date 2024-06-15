8275670dd195d98385b768a5f40617f66e0c5141
DAN-3200 <Tunes.750@outlook.com>
2024-06-15 11:48:02 -0300

    edit: app.jsx

--- clientSide/src/App.jsx
+++ clientSide/src/App.jsx
@@ -1,4 +1,5 @@
 import { css, cx } from '@linaria/core'
+import { styled } from '@linaria/react'
 import { color } from './theme.style'
 
 // class css global
@@ -9,6 +10,22 @@ const glob = {
 		justify-content: center;
 	`,
 }
+
+const Button = styled.button`
+	background-color: ${color.green[500]};
+	border: none;
+	cursor: pointer;
+	transition: 0.2;
+	height: 40px;
+	width: auto;
+	padding: 4px 16px;
+	border-radius: 12px;
+
+	&:active {
+		transition: 0.2s;
+		transform: scale(0.8);
+	}
+`
 
 export default function App() {
 	// class css local/scoped
@@ -23,8 +40,8 @@ export default function App() {
 		<>
 			<div
 				className={css`
-					width: 800px;
-					height: 420px;
+					width: 960px;
+					height: 520px;
 					display: grid;
 					grid-template-columns: repeat(4, 1fr);
 					grid-template-rows: repeat(3, 1fr);
@@ -33,14 +50,30 @@ export default function App() {
 					border-radius: 16px;
 
 					& > div {
-						background-color: ${color.white[500]};
+						background-color: white;
+					}
+
+					& > div:nth-child(1) {
+						grid-area: 1 / 1 / 2 / 4;
+					}
+					& > div:nth-child(2) {
+						grid-area: 2 / 1 / 4 / 3;
+					}
+					& > div:nth-child(3) {
+						grid-area: 1 / 4 / 3 / 5;
+					}
+					& > div:nth-child(4) {
+						grid-area: 2 / 3 / 4 / 4;
+					}
+					& > div:nth-child(5) {
+						grid-area: 3 / 4 / 4 / 5;
 					}
 				`}
 			>
 				<div
 					className={cx(
-						loc.center,
-						glob.bolder,
+						glob.center,
+						loc.bolder,
 						css`
 							grid-area: 1 / 1 / 2 / 4;
 						`
@@ -49,6 +82,12 @@ export default function App() {
 					Linaria
 				</div>
 				<div className={glob.center}>Lemure</div>
+				<div className={glob.center}>
+					<Button>AAAAAAAAAAAA</Button>
+				</div>
+				<div></div>
+				<div></div>
+				<div></div>
 			</div>
 		</>
 	)
